39450c1fa36e693f09d0ab575b420bbde3c61533
vinypaulino <vinydos3@gmail.com>
2018-02-23 17:31:02 -0400

    Criado metodo em Interface para pegar os dados do Servidor e Criado classe AlunoSync para recuperar a resposta do server com a lista de alunos e uma string data de modificação

--- app/src/main/java/br/com/vinipaulino/mobile/agenda/service/AlunoService.java
+++ app/src/main/java/br/com/vinipaulino/mobile/agenda/service/AlunoService.java
@@ -1,8 +1,10 @@
 package br.com.vinipaulino.mobile.agenda.service;
 
 import br.com.vinipaulino.mobile.agenda.modelo.Aluno;
+import br.com.vinipaulino.mobile.agenda.tdo.AlunoSync;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 /**
@@ -12,4 +14,7 @@ import retrofit2.http.POST;
 public interface AlunoService {
     @POST("aluno")
     Call<Void> insere(@Body Aluno aluno);
+
+    @GET("aluno")
+    Call<AlunoSync> lista();
 }
---
+++ app/src/main/java/br/com/vinipaulino/mobile/agenda/tdo/AlunoSync.java
@@ -0,0 +1,30 @@
+package br.com.vinipaulino.mobile.agenda.tdo;
+
+import java.util.List;
+
+import br.com.vinipaulino.mobile.agenda.modelo.Aluno;
+
+/**
+ * Created by vinyp on 23/02/2018.
+ */
+
+public class AlunoSync {
+    private List<Aluno> alunos;
+    private String momentoDaUltimaModificacao;
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(List<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+
+    public String getMomentoDaUltimaModificacao() {
+        return momentoDaUltimaModificacao;
+    }
+
+    public void setMomentoDaUltimaModificacao(String momentoDaUltimaModificacao) {
+        this.momentoDaUltimaModificacao = momentoDaUltimaModificacao;
+    }
+}

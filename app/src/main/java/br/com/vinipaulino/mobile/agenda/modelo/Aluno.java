27e5ad97c0f9ab299eb6c64eff0f9bf47c1e34a4
vinypaulino <vinydos3@gmail.com>
2018-02-23 16:47:26 -0400

    Alterado tabela Alunos para CHAR(36) e configurado UUID para is ids dos alunos

--- app/src/main/java/br/com/vinipaulino/mobile/agenda/modelo/Aluno.java
+++ app/src/main/java/br/com/vinipaulino/mobile/agenda/modelo/Aluno.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
  */
 
 public class Aluno implements Serializable{
-    private Long id;
+    private String id;
     private String nome;
     private String endereco;
     private String telefone;
@@ -23,11 +23,11 @@ public class Aluno implements Serializable{
         this.caminhoFoto = caminhoFoto;
     }
 
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
--- app/src/main/java/br/com/vinipaulino/mobile/agenda/retrofit/RetrofitInicializador.java
+++ app/src/main/java/br/com/vinipaulino/mobile/agenda/retrofit/RetrofitInicializador.java
@@ -1,6 +1,8 @@
 package br.com.vinipaulino.mobile.agenda.retrofit;
 
 import br.com.vinipaulino.mobile.agenda.service.AlunoService;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
@@ -9,16 +11,24 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
  */
 
 public class RetrofitInicializador {
+
     private final Retrofit retrofit;
 
-    public  RetrofitInicializador(){
+    public RetrofitInicializador() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient.Builder client = new OkHttpClient.Builder();
+        client.addInterceptor(interceptor);
+
         retrofit = new Retrofit.Builder()
-        .baseUrl("http://192.168.25.42:8080/api/")
-        .addConverterFactory(JacksonConverterFactory.create())
-        .build();
+                .baseUrl("http://192.168.25.42:8080/api/")
+                .addConverterFactory(JacksonConverterFactory.create())
+                .client(client.build())
+                .build();
     }
 
     public AlunoService getAlunoService() {
-        return  retrofit.create(AlunoService.class);
+        return retrofit.create(AlunoService.class);
     }
 }
